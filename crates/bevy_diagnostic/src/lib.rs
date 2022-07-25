94a18375cfc3149d2bb5a557fdf5f29c39d75d67
IceSentry <c.giguere42@gmail.com>
2022-07-25 13:43:42 -0400

    log system info on startup

--- crates/bevy_diagnostic/src/lib.rs
+++ crates/bevy_diagnostic/src/lib.rs
@@ -2,12 +2,14 @@ mod diagnostic;
 mod entity_count_diagnostics_plugin;
 mod frame_time_diagnostics_plugin;
 mod log_diagnostics_plugin;
+use bevy_log::info;
 pub use diagnostic::*;
 pub use entity_count_diagnostics_plugin::EntityCountDiagnosticsPlugin;
 pub use frame_time_diagnostics_plugin::FrameTimeDiagnosticsPlugin;
 pub use log_diagnostics_plugin::LogDiagnosticsPlugin;
 
 use bevy_app::prelude::*;
+use sysinfo::{CpuExt, SystemExt};
 
 /// Adds core diagnostics resources to an App.
 #[derive(Default)]
@@ -15,10 +17,45 @@ pub struct DiagnosticsPlugin;
 
 impl Plugin for DiagnosticsPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<Diagnostics>();
+        app.init_resource::<Diagnostics>()
+            .add_startup_system(log_system_information);
     }
 }
 
 /// The width which diagnostic names will be printed as
 /// Plugin names should not be longer than this value
 pub const MAX_DIAGNOSTIC_NAME_WIDTH: usize = 32;
+
+#[derive(Debug)]
+// This is required because the Debug trait doesn't detect it's used when it's only used in a print :(
+#[allow(dead_code)]
+struct SystemInfo {
+    os: String,
+    kernel: String,
+    cpu: String,
+    core_count: String,
+    memory: String,
+}
+
+fn log_system_information() {
+    let mut sys = sysinfo::System::new();
+    sys.refresh_cpu();
+    sys.refresh_memory();
+
+    let info = SystemInfo {
+        os: sys
+            .long_os_version()
+            .unwrap_or_else(|| String::from("not available")),
+        kernel: sys
+            .kernel_version()
+            .unwrap_or_else(|| String::from("not available")),
+        cpu: sys.global_cpu_info().brand().trim().to_string(),
+        core_count: sys
+            .physical_core_count()
+            .map(|x| x.to_string())
+            .unwrap_or_else(|| String::from("not available")),
+        memory: format!("{} KB", sys.total_memory()),
+    };
+
+    info!("{:?}", info);
+}
