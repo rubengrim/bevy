4c631b2eccfd71f83379cb441b9cfd4603fd23a3
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-04-14 20:37:55 -0400

    Add TODO for blas build

--- crates/bevy_solari/src/blas.rs
+++ crates/bevy_solari/src/blas.rs
@@ -31,6 +31,7 @@ pub fn prepare_blas(
     render_queue: Res<RenderQueue>,
 ) {
     // Get GpuMeshes and filter to TriangleList meshes without an existing BLAS
+    // TODO: Filter by meshes with position as their first attribute. Otherwise things break.
     let meshes = meshes
         .iter()
         .filter_map(|mesh| render_meshes.get(mesh).map(|gpu_mesh| (mesh, gpu_mesh)))
