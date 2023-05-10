196b3f95173d8bc94f73e1861d6b07be00394204
JMS55 <47158642+JMS55@users.noreply.github.com>
2023-05-10 16:42:24 -0400

    Add pipeline running

--- crates/bevy_solari/src/path_tracer/node.rs
+++ crates/bevy_solari/src/path_tracer/node.rs
@@ -1,6 +1,10 @@
 use super::{
     camera::SolariPathTracer,
-    pipeline::{SolariPathTracerPipelineId, SolariPathtracerPipeline, TraceRaysFromBufferId},
+    pipeline::{
+        CheckOrderKey32Id, GenerateKey32Id, MapArrayKey32PassId, PrefixSumFirstPassId,
+        PrefixSumSecondPassId, PrefixSumThirdPassId, SolariPathTracerPipelineId,
+        SolariPathtracerPipeline, TraceRaysFromBufferId,
+    },
     resources::{create_view_bind_group, SolariPathTracerAccumulationTexture},
 };
 use crate::scene::bind_group::SolariSceneBindGroup;
@@ -22,6 +26,12 @@ pub struct SolariPathTracerNode(
         &'static SolariPathTracer,
         &'static SolariPathTracerPipelineId,
         &'static TraceRaysFromBufferId,
+        &'static GenerateKey32Id,
+        &'static CheckOrderKey32Id,
+        &'static PrefixSumFirstPassId,
+        &'static PrefixSumSecondPassId,
+        &'static PrefixSumThirdPassId,
+        &'static MapArrayKey32PassId,
         &'static SolariPathTracerAccumulationTexture,
         &'static ViewTarget,
         &'static ViewUniformOffset,
@@ -37,7 +47,7 @@ impl Node for SolariPathTracerNode {
         world: &World,
     ) -> Result<(), NodeRunError> {
         let (
-            Ok((path_tracer, pipeline_id,tr_id, accumulation_texture, view_target, view_uniform_offset, camera)),
+            Ok((path_tracer, pipeline_id, tr_id, gen_key32_id, check_key32_id, ps_first_id, ps_second_id, ps_third_id, map_key32_id, accumulation_texture, view_target, view_uniform_offset, camera)),
             Some(pipeline_cache),
             Some(SolariSceneBindGroup(Some(scene_bind_group))),
             Some(view_uniforms),
@@ -51,13 +61,33 @@ impl Node for SolariPathTracerNode {
         ) else {
             return Ok(());
         };
-        let (Some(pipeline), Some(tr_pipeline), Some(viewport)) = (pipeline_cache.get_compute_pipeline(pipeline_id.0), pipeline_cache.get_compute_pipeline(tr_id.0), camera.physical_viewport_size) else {
+        let (
+            Some(pipeline),
+            Some(tr_pipeline),
+            Some(gen_key32_pipeline),
+            Some(check_ky32_pipeline),
+            Some(ps_first_pipeline),
+            Some(ps_second_pipeline),
+            Some(ps_third_pipeline),
+            Some(map_key32_pipeline),
+            Some(viewport),
+        ) = (
+            pipeline_cache.get_compute_pipeline(pipeline_id.0),
+            pipeline_cache.get_compute_pipeline(tr_id.0),
+            pipeline_cache.get_compute_pipeline(gen_key32_id.0),
+            pipeline_cache.get_compute_pipeline(check_key32_id.0),
+            pipeline_cache.get_compute_pipeline(ps_first_id.0),
+            pipeline_cache.get_compute_pipeline(ps_second_id.0),
+            pipeline_cache.get_compute_pipeline(ps_third_id.0),
+            pipeline_cache.get_compute_pipeline(map_key32_id.0),
+            camera.physical_viewport_size,
+        ) else {
             return Ok(());
         };
-        let Some(view_bind_group1) = create_view_bind_group(view_uniforms, accumulation_texture, view_target, solari_pipeline, render_context.render_device(), false) else {
+        let Some(mut view_bind_group1) = create_view_bind_group(view_uniforms, accumulation_texture, view_target, solari_pipeline, render_context.render_device(), false) else {
             return Ok(());
         };
-        let Some(view_bind_group2) = create_view_bind_group(view_uniforms, accumulation_texture, view_target, solari_pipeline, render_context.render_device(), true) else {
+        let Some(mut view_bind_group2) = create_view_bind_group(view_uniforms, accumulation_texture, view_target, solari_pipeline, render_context.render_device(), true) else {
             return Ok(());
         };
 
@@ -74,6 +104,15 @@ impl Node for SolariPathTracerNode {
             solari_pass.set_pipeline(pipeline);
             solari_pass.set_push_constants(0, &previous_sample_count.to_le_bytes());
             solari_pass.dispatch_workgroups((viewport.x + 7) / 8, (viewport.y + 7) / 8, 1);
+
+            let mut starting_bit = 32;
+            while starting_bit > 0 {
+                // TODO: Run passes
+
+                std::mem::swap(&mut view_bind_group1, &mut view_bind_group2);
+                solari_pass.set_bind_group(1, &view_bind_group1, &[view_uniform_offset.offset]);
+                starting_bit -= 2;
+            }
 
             solari_pass.set_pipeline(tr_pipeline);
             solari_pass.dispatch_workgroups((viewport.x + 7) / 8, (viewport.y + 7) / 8, 1);
